79744ccb898637f34e9b89e8e710cdc34e5ba53e
canogerman <germancano80@gmail.com>
2023-05-25 10:40:51 -0300

    add Filter_books.js file with 2nd part of exercise

--- Filter_books.js
+++ Filter_books.js
@@ -1,38 +1,41 @@
-// Primera parte: dado un array de libros, implementar una función filterBooksByAuthor( ) que reciba por parámetros
-// dicho array y un string con el nombre de un autor, y devuelva un nuevo array con libros del autor indicado.
 
-    const books = [
-        { title: 'El Aleph', author: 'Jorge Luis Borges' },
-        { title: 'La ciudad y los perros', author: 'Mario Vargas Llosa' },
-        { title: 'Cien años de soledad', author: 'Gabriel García Márquez' },
-        { title: 'Rayuela', author: 'Julio Cortázar' },
-        { title: 'Ficciones', author: 'Jorge Luis Borges' },
-        { title: 'El hacedor', author: 'Jorge Luis Borges' },
-        { title: 'Los pasos perdidos', author: 'Alejo Carpentier' },
-        { title: 'El reino de este mundo', author: 'Alejo Carpentier' },
-        { title: 'La fiesta del chivo', author: 'Mario Vargas Llosa' },
-        { title: 'La tía Julia y el escribidor', author: 'Mario Vargas Llosa' },
-        { title: 'Crónica de una muerte anunciada', author: 'Gabriel García Márquez' },
-        { title: 'El amor en los tiempos del cólera', author: 'Gabriel García Márquez' },
-        { title: 'Bestiario', author: 'Julio Cortázar' },
-        { title: 'Las armas secretas', author: 'Julio Cortázar' }
-      ];
+
+const books = [
+    { title: 'El Aleph', author: 'Jorge Luis Borges' },
+    { title: 'La ciudad y los perros', author: 'Mario Vargas Llosa' },
+    { title: 'Cien años de soledad', author: 'Gabriel García Márquez' },
+    { title: 'Rayuela', author: 'Julio Cortázar' },
+    { title: 'Ficciones', author: 'Jorge Luis Borges' },
+    { title: 'El hacedor', author: 'Jorge Luis Borges' },
+    { title: 'Los pasos perdidos', author: 'Alejo Carpentier' },
+    { title: 'El reino de este mundo', author: 'Alejo Carpentier' },
+    { title: 'La fiesta del chivo', author: 'Mario Vargas Llosa' },
+    { title: 'La tía Julia y el escribidor', author: 'Mario Vargas Llosa' },
+    { title: 'Crónica de una muerte anunciada', author: 'Gabriel García Márquez' },
+    { title: 'El amor en los tiempos del cólera', author: 'Gabriel García Márquez' },
+    { title: 'Bestiario', author: 'Julio Cortázar' },
+    { title: 'Las armas secretas', author: 'Julio Cortázar' }
+];
       
 function filterBooksByAuthor(books, author) {
     const filteredBooks = books.filter(book => book.author === author);
-    console.log(filteredBooks);
+    const modifiedBooks = filteredBooks.map(book => {
+        book.author = 'J.L. Borges';
+        return book;  //this modify the original "book" array . 
+    });
+    console.log(modifiedBooks);
 };
 
 filterBooksByAuthor(books, 'Jorge Luis Borges');
+console.log(books);
 
-
-
+// Primera parte: dado un array de libros, implementar una función filterBooksByAuthor( ) que reciba por parámetros
+// dicho array y un string con el nombre de un autor, y devuelva un nuevo array con libros del autor indicado.
 // filter Books By Author(books, ‘Jorge Luis Borges’) return ==> [
 //   { title: 'El Aleph', author: 'Jorge Luis Borges' },
 //   { title: 'Ficciones', author: 'Jorge Luis Borges' },
 //   { title: 'El hacedor', author: 'Jorge Luis Borges' }
 // ];
-
 
 // Segunda parte: a partir de lo realizado en la primera parte, utilizar la función para obtener todos los libros
 // de un autor a elección, luego mediante métodos de array modificar el nombre del autor de todos los libros filtrados.
@@ -41,7 +44,6 @@ filterBooksByAuthor(books, 'Jorge Luis Borges');
 
 // Tercera parte: modificar la función de la primera parte para que ahora funcione si mando el nombre completo del autor,
 // o solo su apellido. Por ejemplo, la función debe devolver lo siguiente, ya sea si le mando ‘Jorge Luis Borges’ o ‘Borges’:
-
 // return ==> [
 //   { title: 'El Aleph', author: 'Jorge Luis Borges' },
 //   { title: 'Ficciones', author: 'Jorge Luis Borges' },
